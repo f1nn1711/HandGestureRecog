124cb6bfd86d33fbc083d9455dc3ac3c2f95d386
Finn Hunt <hunt.finn@gmail.com>
2021-09-05 23:08:40 +0100

    added video capture
    added video capture and shows it in a window
    also added a config file to allow better control of certain parameters

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 #pip install -r requirements.txt
 #or
 #pip3 install -r requirements.txt
+import cv2
+import json
+
+with open('config.txt', 'r') as f:
+    configOpts = json.load(f)
+
+capture = cv2.VideoCapture(0)
+
+while capture.isOpened():
+    ret, img = capture.read()
+
+    if configOpts['greyscale']:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    if configOpts['flip']:
+        img = cv2.flip(img, 1)
+
+    cv2.imshow('window', img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
